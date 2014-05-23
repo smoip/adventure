988813637e014efc537c7e8d43403755e85c6142
smoip <Laevsky@robertsmacbook4.gateway.pace.com>
2014-05-23 17:07:52 -0500

    cleaning up file links

--- adventure.rb
+++ adventure.rb
@@ -6,7 +6,11 @@
 # In a future version, handles all IO issues (remove puts, print from internal objects)
 
 $LOAD_PATH.insert(0, "/users/laevsky/documents/learning ruby/adventure")
-require (character.rb, dm.rb, map.rb, magic.rb, items.rb)
+require "character.rb"
+require "dm.rb"
+require "map.rb"
+require "magic.rb"
+require "items.rb"
 
 
 class Game
@@ -16,3 +20,34 @@ class Game
 	end
 	
 end
+
+puts 'Please enter a name for a new fighter.'	
+playerChar = Fighter.new((gets.chomp.downcase.capitalize), 0)
+puts "Name: #{playerChar.name}"
+puts "MP: #{playerChar.currentMP}"
+puts playerChar.statusCheck.to_s
+
+playerChar.hp=(-5)
+puts "Ouch. HP now: #{playerChar.currentHP}"
+
+playerChar.hp=(10)
+puts "Much better. HP now: #{playerChar.currentHP}"
+
+playerChar.hp=(-11)
+playerChar.statusCheck
+
+computerChar = Mage.new('Elrich', 1)
+puts computerChar.statusCheck
+
+playerChar.castSpell('heal', playerChar)
+playerChar.attack(computerChar)
+computerChar.castSpell('fireball', playerChar)
+playerChar.attack(computerChar)
+computerChar.attack(playerChar)
+playerChar.attack(computerChar)
+computerChar.attack(playerChar)
+computerChar.attack(playerChar)
+puts computerChar.statusCheck.to_s
+
+playerChar.gainExp(20)
+puts playerChar.statusCheck.to_s
--- character.rb
+++ character.rb
@@ -192,33 +192,3 @@ end
 
 
 
-puts 'Please enter a name for a new fighter.'	
-playerChar = Fighter.new((gets.chomp.downcase.capitalize), 0)
-puts "Name: #{playerChar.name}"
-puts "MP: #{playerChar.currentMP}"
-puts playerChar.statusCheck.to_s
-
-playerChar.hp=(-5)
-puts "Ouch. HP now: #{playerChar.currentHP}"
-
-playerChar.hp=(10)
-puts "Much better. HP now: #{playerChar.currentHP}"
-
-playerChar.hp=(-11)
-playerChar.statusCheck
-
-computerChar = Mage.new('Elrich', 1)
-puts computerChar.statusCheck
-
-playerChar.castSpell('heal', playerChar)
-playerChar.attack(computerChar)
-computerChar.castSpell('fireball', playerChar)
-playerChar.attack(computerChar)
-computerChar.attack(playerChar)
-playerChar.attack(computerChar)
-computerChar.attack(playerChar)
-computerChar.attack(playerChar)
-puts computerChar.statusCheck.to_s
-
-playerChar.gainExp(20)
-puts playerChar.statusCheck.to_s
