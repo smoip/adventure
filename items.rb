46136c5d0301c7db4c8ea523731ddc532c5a34c6
smoip <Laevsky@robertsmacbook4.gateway.pace.com>
2014-05-23 16:42:16 -0500

    update and new dependencies
    Updates to character, added mostly empty dependencies to be filled as I
    go

---
+++ items.rb
@@ -0,0 +1,23 @@
+# what does an item do?
+# called by an owner object (character, dm)
+# reports effects to owner
+# owner executes effects
+# are these instantiated objects that exist independently of owner?
+# or are they a library whose effects are called from a module?
+
+class Item
+
+end
+
+class Potion < Item
+
+end
+
+class Weapon < Item
+
+end
+
+class Armor < Item
+
+end
+
---
+++ map.rb
@@ -0,0 +1,8 @@
+# what does map do?
+# generates 'terrain' as character 'moves'
+# rather than tracking the location of objects,
+# map tells dm what is present at each square as character moves into it.
+# for now, a square only exists while a character is in it
+# later versions may track explored terrain
+
+# map decides what objects are present in a new square and reports back to dm
